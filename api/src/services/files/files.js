87d1ecaa87c5b7638936cc4383ea1c8095ee6ced
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2023-12-01 21:38:01 +0100

    Conexao com AMAZON AWS, configuracao e envio de file

--- api/src/services/files/files.js
+++ api/src/services/files/files.js
@@ -1,6 +1,18 @@
 import { db } from 'src/lib/db';
 import fs from 'fs/promises';
 import path from 'path';
+import { S3Client, HeadObjectCommand, PutObjectCommand, ListBucketsCommand } from '@aws-sdk/client-s3'
+import {V4 as uuid} from 'uuid'
+
+const s3Client = new S3Client({
+  credentials:{
+    accessKeyId:process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  },
+  region: 'us-east-1'
+})
+
+const bucketName = 'rhf-test-bucket';
 
 export const files = () => {
   return db.file.findMany();
@@ -15,8 +27,41 @@ export const file = ({ id }) => {
 export const createFile = async ({ input }) => {
   const { name, file } = input
 
+  const fileExistsInS3 = async (key) => {
+
+    try {
+      await s3Client.send(
+        new HeadObjectCommand({
+          Bucket: bucketName,
+          Key: name
+      })
+    )
+    return true
+    } catch (error){
+      return false
+    }
+  }
+
+  const uploadFileToS3 = async (key, buffer) => {
+    await s3Client.send(
+      new PutObjectCommand({
+        Bucket: bucketName,
+        Key: key,
+        Body: buffer
+      })
+    )
+  }
+
   const uploadDirectory = path.join(__dirname, '../../../../web/public/uploads')
   const filePath = path.join(uploadDirectory, name)
+
+  const key = name
+  const fileExists = await fileExistsInS3(key)
+
+  if (fileExists) {
+    console.log('O arquivo já existe no S3. Upload ignorado.');
+    return null
+  }
 
   try {
     const fileExists = await fs.access(filePath).then(() => true).catch(() => false)
@@ -34,12 +79,13 @@ export const createFile = async ({ input }) => {
         url: `/uploads/${name}`,
       }
 
-      return db.file.create({
-        data,
-      })
+      const createdFile = db.file.create({data})
 
-    }else {
-      console.log('O arquivo já existe. Upload ignorado.')
+      await uploadFileToS3(key, file.buffer);
+
+      console.log('Upload para o S3 concluído com sucesso.');
+
+      return createdFile
 
     }
   } catch (error) {
