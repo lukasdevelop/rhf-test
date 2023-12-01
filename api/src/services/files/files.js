2442da04543874401d222f8def43f953c5c0bf52
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2023-12-01 23:06:20 +0100

    Controle de versao no S3

--- api/src/services/files/files.js
+++ api/src/services/files/files.js
@@ -1,8 +1,10 @@
 import { db } from 'src/lib/db';
 import fs from 'fs/promises';
 import path from 'path';
-import { S3Client, HeadObjectCommand, PutObjectCommand, ListBucketsCommand } from '@aws-sdk/client-s3'
+import { S3Client, HeadObjectCommand, PutObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3'
 import {V4 as uuid} from 'uuid'
+import {diffChars} from 'diff';
+
 
 const s3Client = new S3Client({
   credentials:{
@@ -42,11 +44,14 @@ export const createFile = async ({ input }) => {
     }
   }
 
-  const uploadFileToS3 = async (key, buffer) => {
+  const uploadFileToS3 = async (key, buffer, version) => {
+
+    const versionedKey = version ? `${key}-v${version}` : key;
+
     await s3Client.send(
       new PutObjectCommand({
         Bucket: bucketName,
-        Key: key,
+        Key: versionedKey,
         Body: buffer
       })
     )
@@ -58,34 +63,50 @@ export const createFile = async ({ input }) => {
   const key = name
   const fileExists = await fileExistsInS3(key)
 
-  if (fileExists) {
-    console.log('O arquivo já existe no S3. Upload ignorado.');
-    return null
-  }
-
   try {
-    const fileExists = await fs.access(filePath).then(() => true).catch(() => false)
+    let createdFile;
+
+   // const fileExists = await fs.access(filePath).then(() => true).catch(() => false)
 
     if(!fileExists){
 
-      await fs.mkdir(uploadDirectory, { recursive: true });
+      await fs.mkdir(uploadDirectory, { recursive: true })
 
-      console.log('Tentando escrever no sistema de arquivos:', filePath);
+      console.log('Tentando escrever no sistema de arquivos:', filePath)
 
       await fs.writeFile(filePath, file.buffer)
 
       const data = {
         name,
         url: `/uploads/${name}`,
+        version: 1
       }
 
-      const createdFile = db.file.create({data})
+       createdFile = db.file.create({data})
 
-      await uploadFileToS3(key, file.buffer);
+      await uploadFileToS3(key, file.buffer, data.version)
 
-      console.log('Upload para o S3 concluído com sucesso.');
+      console.log('Upload para o S3 concluído com sucesso.')
 
       return createdFile
+
+    } else {
+      const previousVersionBuffer = await s3Client.send(
+        new GetObjectCommand({
+          Bucket: bucketName,
+          Key: key,
+        })
+      )
+
+      const differences = diffChars(previousVersionBuffer.toString(), file.buffer.toString())
+
+      if (differences.length > 0) {
+        await fs.writeFile(filePath, file.buffer);
+        const currentVersion = parseInt(previousVersionBuffer.Metadata.version, 10);
+        await uploadFileToS3(key, file.buffer, currentVersion + 1);
+      } else {
+        console.log('Não há diferenças no conteúdo. Upload ignorado.');
+      }
 
     }
   } catch (error) {
